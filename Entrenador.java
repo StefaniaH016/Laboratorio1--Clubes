6676a37c26e3194c4a3417cd87579a4df01b55c6
StefaniaH016 <160367091+StefaniaH016@users.noreply.github.com>
2024-09-01 21:19:02 -0500

    Delete Entrenador.java

--- Entrenador.java
+++
@@ -1,9 +0,0 @@
-package co.laboratorio1.edu;
-
-public class Entrenador {
-
-    private String nombre;
-    private String especialidad;
-    private SesionEntrenamiento sesionesEntrenamientos;
-
-}
